c01d46c810b3dcd9ea9f96fcb7aeddbc32c31a9b
zhangrp <zhangrp@bairuitech.com>
2020-02-02 12:48:10 +0800

    创建记账、标签、统计、404页面。创建路由
    Change-Id: Ie73be6759262b125e43cff572391473decd2fef7

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+      <router-view />
+      <div>
+          <router-link to="/money">记账</router-link> |
+          <router-link to="/labels">标签</router-link> |
+          <router-link to="/statistics">统计</router-link>
+      </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'App'
+}
+</script>
+<style lang="scss">
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        该页面不存在, 请输入正确的路径
+        <br />
+        <router-link to="/">返回首页</router-link>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: "NotFound"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
