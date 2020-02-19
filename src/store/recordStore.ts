a0da6900811bf9b15ffad86d81716c76ab3f0b9e
zhangrp <zhangrp@bairuitech.com>
2020-02-19 17:39:44 +0800

    重构recordStore.ts中
    Change-Id: I86ba4b26a537ec0cfbab5edc852a5276da216cab

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -3,23 +3,21 @@ import clone from '@/lib/clone';
 const localStorageKey = 'recordList';
 let data: RecordItem[] | undefined = undefined;
 
-function fetch() {
-    data = JSON.parse(window.localStorage.getItem(localStorageKey) || '[]') as RecordItem[];
-    return data;
-}
-
-function save() {
-    window.localStorage.setItem(localStorageKey, JSON.stringify(data));
-}
-
-export default {
-    recordList: fetch(),
-    fetch: fetch,
-    save: save,
+const recordStore = {
+    recordList: data,
+    fetch() {
+        data = JSON.parse(window.localStorage.getItem(localStorageKey) || '[]') as RecordItem[];
+        return data;
+    },
+    save() {
+        window.localStorage.setItem(localStorageKey, JSON.stringify(data));
+    },
     createRecord: (record: RecordItem) => {
         let item: RecordItem = clone(record);
         item.createAt = new Date();
         data && data.push(item);
-        save();
+        recordStore.save();
     },
 };
+recordStore.fetch();
+export default recordStore;
--- src/store/tagStore.ts
+++ src/store/tagStore.ts
@@ -1,11 +1,34 @@
 import tagListModel from '@/models/tagListModel';
+import createId from '@/lib/createId';
+
+const localStorageKey = 'tagList';
+let data = undefined;
+
+function fetch() { // 取
+    data = JSON.parse(window.localStorage.getItem(localStorageKey) || '[]');
+    return data;
+}
+function create() {
+    // data --- [{id: xxx, name: xxx}]
+    let names = data.map(item => item.name);
+    if (names.indexOf(name) === -1) {
+        let id = createId().toString();
+        data.push({id, name});
+        save();
+        return 'success';
+    } else {
+        return 'duplication';
+    }
+}
+function save() {
+    window.localStorage.setItem(localStorageKey, JSON.stringify(this.data));
+}
 
 export default {
-    // tag store
-    // 将数据统一获取, 其余页面直接使用这里获取的数据即可
-    tagList: tagListModel.fetch(),
+    tagList: fetch(),
+    fetch: fetch,
     createTag: (name: string) => {
-        let success = tagListModel.create(name);
+        let success = create(name);
         if (success === 'success') {
             window.alert('创建成功');
         } else if (success === 'duplication') {
@@ -21,4 +44,4 @@ export default {
     findTag: function (id: string) {
         return this.tagList.filter(tag => tag.id === id)[0];
     }
-}
+};
