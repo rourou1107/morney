7b2456246e930862907ea281293210f691e89264
zhangrp <zhangrp@bairuitech.com>
2020-02-27 10:12:01 +0800

    Statistics.vue使用deep语法
    Change-Id: I9b3ad4433d55cceb0c1a09d68fea784f1df2c48f

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,14 +1,29 @@
 <template>
-    <layout>statistics</layout>
+    <layout>
+        <Types :value.sync="type" class="xxx"/>
+    </layout>
 </template>
 
 <script lang="ts">
+    import Vue from 'vue';
     import Layout from "@/components/Layout.vue";
-    export default {
-        name: "Statistics",
-        components: {Layout}
+    import {Component} from 'vue-property-decorator';
+    import Types from '@/components/money/Types.vue';
+    @Component({
+        components: {Types, Layout}
+    })
+    export default class Statistics extends Vue {
+        type:string =  '-';
     }
 </script>
 
 <style lang="scss" scoped>
+    .xxx ::v-deep div { /* >>> 等价于 ::v-deep */
+        &.selected {
+            background: #ffffff;
+            &::after {
+                display: none;
+            }
+        }
+    }
 </style>
