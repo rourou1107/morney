6be2bd3e60d389e8249349526b47dcc45404d16d
zhangrp <zhangrp@bairuitech.com>
2020-03-01 16:32:41 +0800

    statistic.vue 完成
    Change-Id: I34acb98933176f8c36707d63f504d4c973327cdd

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -4,12 +4,15 @@
               :value.sync="type"
               class-prefix="type"
         />
-        <Tabs :data-source="intervalList"
-              :value.sync="interval"
-              class-prefix="interval"
-        />
-        <div v-for="group in result" :key="group.title">
-            <h3 class="title">{{beautifyTime(group.title)}}</h3>
+        <div v-for="group in groupList" :key="group.title">
+            <h3 class="title">
+                {{beautifyTime(group.title)}}
+                <span :class="{orange: type==='+'}">
+                    合计:
+                    <span class="mark">{{type==='+' ? '+' : '-'}}</span>
+                    <span>{{group.total}} 元</span>
+                </span>
+            </h3>
             <ol>
                 <li v-for="(item, index) in group.items" :key="index" class="record">
                     <span>{{tagString(item.tags)}}</span>
@@ -29,7 +32,6 @@
     import Layout from '@/components/Layout.vue';
     import {Component} from 'vue-property-decorator';
     import Tabs from '@/components/Tabs.vue';
-    import intervalList from '@/constants/intervalList';
     import typeList from '@/constants/typeList';
     import dayjs from 'dayjs';
     import clone from '@/lib/clone';
@@ -40,8 +42,6 @@
     })
     export default class Statistics extends Vue {
         type: string = '-';
-        interval: string = 'day';
-        intervalList = intervalList;
         typeList = typeList;
 
         get recordList() {
@@ -49,13 +49,14 @@
             return (this.$store.state as myState).recordList;
         }
 
-        get result() {
-            type hashTableValue = {
+        get groupList() {
+            type ResultListValue = {
                 title: string,
-                items: RecordItem[]
+                items: RecordItem[],
+                total?: number
             }
             /**
-             * hashTable = [
+             * resultList = [
              * {title: 2020-02-27,
              *  items: [
              *           {tags: ['衣'], notes: '买衣服', type: '-', amount: 120, createAt: '2020-02-27T01:32:58.518Z'},
@@ -63,15 +64,29 @@
              *         ]},
              *     .........]
              **/
-            const hashTable: { [key: string]: hashTableValue }[] = [];
+            const resultList: ResultListValue[] = [];
             const recordList = this.recordList;
-            // for (let i = 0; i < recordList.length; i++) {
-            //     const [date] = recordList[i].createAt!.split('T');
-            //     hashTable[date] = hashTable[date] || {title: date, items: []};
-            //     hashTable[date].items.push(recordList[i]);
-            // }
-            const newList = clone(recordList).sort((a, b) => dayjs(b.createAt).valueOf() - dayjs(a.createAt).valueOf());
-            // return hashTable;
+            const newList = clone(recordList).filter(r => r.types === this.type).sort((a, b) => dayjs(b.createAt).valueOf() - dayjs(a.createAt).valueOf());
+            // console.log(newList);
+            resultList[0] = {title: dayjs(newList[0].createAt).format('YYYY-MM-DD'), items: [newList[0]]};
+            for (let i = 1; i < newList.length; i++) {
+                let last = resultList[resultList.length - 1].title;
+                if (dayjs(newList[i].createAt).format('YYYY-MM-DD') === last) {
+                    resultList[resultList.length - 1].items.push(newList[i]);
+                } else {
+                    resultList[resultList.length] = {
+                        title: dayjs(newList[i].createAt).format('YYYY-MM-DD'),
+                        items: [newList[i]]
+                    };
+                }
+            }
+            resultList.forEach((item) => {
+                item.total = item.items.reduce((sum, current) => {
+                    return sum + current.amount;
+                }, 0);
+            });
+            console.log(resultList);
+            return resultList;
         }
 
         created() {
@@ -91,9 +106,9 @@
                 return '昨天';
             } else if (date.isSame(now.subtract(2, 'day'), 'day')) {
                 return '前天';
-            } else if(date.isSame(now, 'year')) {
+            } else if (date.isSame(now, 'year')) {
                 return date.format('M月D日');
-            }else {
+            } else {
                 return date.format('YYYY年M月D日');
             }
         }
@@ -103,11 +118,9 @@
 <style lang="scss" scoped>
     ::v-deep {
         .type-tabs-item { /* >>> 等价于 ::v-deep */
-            background: #ffffff;
-
+            background: #c4c4c4;
             &.selected {
-                background: #c4c4c4;
-
+                background: #ffffff;
                 &::after {
                     display: none;
                 }
@@ -129,6 +142,7 @@
     .title {
         @extend %item;
         line-height: 24px;
+        font-size: 16px;
     }
 
     .record {
@@ -140,11 +154,6 @@
             margin-left: 16px;
             color: #999999;
         }
-
-        .orange {
-            color: #da9b29;
-        }
-
         .amount {
             display: flex;
             align-items: center;
@@ -153,5 +162,8 @@
                 padding-right: 3px;
             }
         }
+    }
+    .orange {
+        color: #da9b29;
     }
 </style>
